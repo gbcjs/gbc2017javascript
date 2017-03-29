c48cef7f3677e67ab60345a84c3581f71b8dc4c0
Naqi Rizvi <rizvisyednaqi@gmail.com>
2017-03-29 17:31:04 -0400

    week 4 files

---
+++ Week 4/conditions.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function myFunction() {
+
+	var input = document.getElementById('input').value;
+
+	//Determine whether the input is an even number
+
+
+
+	//Determine whether the input is an even or odd number
+
+
+
+	//Determine whether the input is neither
+
+
+
+	//Determine whether input isNaN at end
+	
+
+
+}
