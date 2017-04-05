a80f4c7974749c6cd5427ca7d0113c5f11254acf
Naqi Rizvi <rizvisyednaqi@gmail.com>
2017-04-05 17:02:46 -0400

    week 5 files

---
+++ Week 5/addition.js
@@ -0,0 +1,7 @@
+var inputA = document.getElementById('inputA');
+var inputB = document.getElementById('inputB');
+var output = document.getElementById('output');
+
+inputA.addEventListener("input", function () {
+
+})
